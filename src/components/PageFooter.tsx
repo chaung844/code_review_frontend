5c2f4cf2b24ac76701992620e78591c291c2a458
Chau Nguyen <mchaung844@gmail.com>
2023-11-05 10:50:06 -0500

    feat: added footer description

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -2,7 +2,7 @@ export function PageFooter() {
     return (
         <div className="flex flex-col h-[18vh] text-white bg-black justify-center items-center">
             <h1 className="text-2xl font-bold">CodeGenius</h1>
-            <p className="">Created by: </p>
+            <p>@ MakeUC Hackathon 2023</p>
         </div>
     )
 }
