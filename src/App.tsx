c414d5b27ae311560ea2f10b3d8d866e6efee470
Chau Nguyen <mchaung844@gmail.com>
2023-11-05 10:47:32 -0500

    feat: retrived and displayed output from server

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { NavBar } from './components/NavBar';
 import { Layout } from 'antd';
 import { Hero } from './components/Hero';
 import { VulnerabilityCheck } from './components/VulnerabilityCheck';
+import { PageFooter } from './components/PageFooter';
 
-const { Footer, Content } = Layout;
+const { Content } = Layout;
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
           <Content>
              <VulnerabilityCheck />
           </Content>
-          <Footer>
-          </Footer>
+          <PageFooter />
       </Layout>
   );
 }
---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,8 @@
+export function PageFooter() {
+    return (
+        <div className="flex flex-col h-[18vh] text-white bg-black justify-center items-center">
+            <h1 className="text-2xl font-bold">CodeGenius</h1>
+            <p className="">Created by: </p>
+        </div>
+    )
+}
